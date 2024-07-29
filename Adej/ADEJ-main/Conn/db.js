d743ec6d67fbe50890b39670cd8475101088bc00
Bruno Franzosi <brunofranzosi230@hotmail.com>
2024-07-29 16:31:07 -0300

    yeh

--- Adej/ADEJ-main/Conn/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-// Configuração da conexão com o banco de dados MySQL
-const sequelize = new Sequelize('ADEJ', 'root', '', {
-  dialect: 'mysql', 
-  host: 'localhost',
-});
-
-module.exports = sequelize;
--- Adej/ADEJ-main/Middlewares/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    if (req.session && req.session.volunteerId) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  };
-  
